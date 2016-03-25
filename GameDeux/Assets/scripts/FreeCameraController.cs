97c93d939192c4562314222937c62eac703051fe
maxpeabody <mdp4566@rit.edu>
2016-03-24 20:56:27 -0400

    Momentum + control changes (EXPERIMENTAL)
    NOTE: I was unable to figure out how to run the game -- the play button
    in Unity wasn't doing anything, for whatever reason. DO NOT ASSUME THIS
    CODE WILL WORK BEFORE TESTING IT. In particular, Q and E may be
    reversed.
    
    Q and E should rotate the view horizontally, R rises, F descends.
    
    No vertical view rotation is implemented, so we may need to figure out
    what keys we want to use for that, if any? (Since rising/descending
    isn't based on which way you're facing, though, we should be able to
    just put everything "within view" and not worry about it.)

--- GameDeux/Assets/scripts/FreeCameraController.cs
+++ GameDeux/Assets/scripts/FreeCameraController.cs
@@ -14,18 +14,22 @@ public class FreeCameraController : MonoBehaviour
  
 	FEATURES
 		WASD/Arrows:    Movement
-		          Q:    Climb
-		          E:    Drop
+		          Q:    Rotate counterclockwise
+		          E:    Rotate clockwise
+				  R:	Climb
+				  F:	Drop
                       Shift:    Move faster
                     Control:    Move slower
                         End:    Toggle cursor locking to screen (you can also press Ctrl+P to toggle play mode on and off).
 	*/
 	
 	public float cameraSensitivity = 90;
-	public float climbSpeed = 4;
-	public float normalMoveSpeed = 10;
-	public float slowMoveFactor = 0.25f;
-	public float fastMoveFactor = 3;
+	public float maxClimbSpeed = 4;
+	public float maxMoveSpeed = 10;
+	/* public float slowMoveFactor = 0.25f;
+	public float fastMoveFactor = 3; */
+	public float currentVelocityOrthogonal = 0;
+	public float currentVelocityVertical = 0;
 	
 	private float rotationX = 0.0f;
 	private float rotationY = 0.0f;
@@ -37,32 +41,65 @@ public class FreeCameraController : MonoBehaviour
 	
 	void Update ()
 	{
-		rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
+		// Rotation
+		/* rotationX += Input.GetAxis("Mouse X") * cameraSensitivity * Time.deltaTime;
 		rotationY += Input.GetAxis("Mouse Y") * cameraSensitivity * Time.deltaTime;
-		rotationY = Mathf.Clamp (rotationY, -90, 90);
+		rotationY = Mathf.Clamp (rotationY, -90, 90); */
+		if(Input.GetKey(KeyCode.Q))
+			rotationX += 0.12f * cameraSensitivity * Time.deltaTime;
+		else if(Input.GetKey(KeyCode.E))
+			rotationX -= 0.12f * cameraSensitivity * Time.deltaTime;
 		
 		transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-		transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+		// transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 		
+		// Acceleration
 		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
 		{
-			transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-			transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+			/* transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime; */
+			
+			if(currentVelocityOrthogonal < maxMoveSpeed)
+			{
+				currentVelocityOrthogonal += transform.forward * 0.12f * Input.GetAxis("Vertical");
+				currentVelocityOrthogonal += transform.right * 0.08f * Input.GetAxis("Horizontal");
+			}
 		}
+		// Deceleration
 		else if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl))
 		{
-			transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-			transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+			/* transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+			transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * Input.GetAxis("Horizontal") * Time.deltaTime; */
+			
+			if(currentVelocityOrthogonal > -maxMoveSpeed)
+			{
+				currentVelocityOrthogonal -= transform.forward * 0.12f * Input.GetAxis("Vertical");
+				currentVelocityOrthogonal -= transform.right * 0.08f * Input.GetAxis("Horizontal");
+			}
 		}
+			
+		transform.position += currentVelocityOrthogonal * Time.deltaTime;
+		
+		// Rising
+		if (Input.GetKey (KeyCode.R) && currentVelocityVertical < maxClimbSpeed) 
+		{
+			currentVelocityVertical += transform.up * 0.2f;
+		}
+		// Descending
+		else if (Input.GetKey (KeyCode.F) && currentVelocityVertical > -maxClimbSpeed) 
+		{
+			currentVelocityVertical -= transform.up * 0.2f;
+		}
+		// Neither rising nor descending -- slowly decelerate vertical movement
 		else
 		{
-			transform.position += transform.forward * normalMoveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-			transform.position += transform.right * normalMoveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+			if(currentVelocityVertical > 0)
+				currentVelocityVertical -= transform.up * 0.05f;
+			else if(currentVelocityVertical < 0)
+				currentVelocityVertical += transform.up * 0.05f;
 		}
 		
-		
-		if (Input.GetKey (KeyCode.Q)) {transform.position += transform.up * climbSpeed * Time.deltaTime;}
-		if (Input.GetKey (KeyCode.E)) {transform.position -= transform.up * climbSpeed * Time.deltaTime;}
+		transform.position += currentVelocityVertical * Time.deltaTime;
 		
 		if (Input.GetKeyDown (KeyCode.End))
 		{
