4918f3706f7f384b72454f12231a72a1b28ba28c
Isaac Selleck <ixs5214@g.rit.edu>
2016-04-08 00:55:51 -0400

    Added bullets that will knock asteroids around

---
+++ GameDeux/Assets/scripts/BulletCollsion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletCollsion : MonoBehaviour {
+
+	public float pauseAmnt;
+
+
+	void Start(){
+		Destroy (this.gameObject, 5f);
+	}
+
+	void OnCollisionEnter(Collision col){
+
+		if(col.gameObject.tag == "Asteroid"){
+			Destroy (col.gameObject, pauseAmnt);
+				Destroy (this.gameObject);
+		
+		}
+	}
+		
+}
---
+++ GameDeux/Assets/scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public Rigidbody projectile; 
+	public float speed = 20;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Fire1")){
+
+			Rigidbody istantiatedProj = Instantiate (projectile, transform.position, transform.rotation)as Rigidbody;
+
+			Physics.IgnoreCollision (istantiatedProj.GetComponent<Collider>(), GetComponent<Collider>());
+			istantiatedProj.velocity = transform.TransformDirection (new Vector3(0,0,speed));
+
+		}
+
+	}
+
+}
